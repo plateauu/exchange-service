16dcacf93d32f83e6a73c84d343d1239dfa1f859
plateauu <plateauu@o2.pl>
2020-07-28 21:42:08 +0200

    Added get account balance endpoint

--- src/main/kotlin/dev/insidemind/bank/api/BankAccountController.kt
+++ src/main/kotlin/dev/insidemind/bank/api/BankAccountController.kt
@@ -1,13 +1,15 @@
 package dev.insidemind.bank.api
 
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.NumberFormat
+import java.util.*
 
+//TODO Add TypeConverter for BigDecimal with NumberFormat (comma separated values and round to two signs)
 @Controller("/account")
 class BankAccountController {
     private val logger: Logger = LoggerFactory.getLogger(BankAccountController::class.java )
@@ -15,10 +17,25 @@ class BankAccountController {
     @Post
     fun create(@Body request: CreateRequest): HttpResponse<CreateResponse> =
             HttpResponse.ok(CreateResponse(request.pesel, request.amount))
+
+    @Get("/{accountId}")
+    fun getAccountBalance(@PathVariable accountId: String): HttpResponse<GetAccountBalanceResponse> {
+        val amountInPln = NumberFormat.getNumberInstance(Locale("PL", "pl")).parse("123,23523")
+        logger.info("amount: $amountInPln")
+        val toSend = BigDecimal(amountInPln.toString()).setScale(2, RoundingMode.HALF_UP)
+        logger.info("to send: $toSend")
+
+        return HttpResponse.ok(GetAccountBalanceResponse(toSend, BigDecimal(40.2)))
+    }
 }
 
+data class GetAccountBalanceResponse(
+        val amountInPln: BigDecimal,
+        val amountInUsd: BigDecimal
+)
+
 data class CreateResponse(
-        val accountNumber: String,
+        val accountId: String,
         val amount: BigDecimal
 )
 
